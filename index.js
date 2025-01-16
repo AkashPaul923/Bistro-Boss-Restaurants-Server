29aa4293924ff99d60e61b458cbe499515c8b90e
AkashPaul923 <akashpaul923@gmail.com>
2025-01-16 16:12:15 +0600

    create jwt token api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -32,6 +33,13 @@ async function run() {
     const menuCollection = client.db("BistroBossRestaurantDB").collection("menu")
     const reviewCollection = client.db("BistroBossRestaurantDB").collection("reviews")
     const cartCollection = client.db("BistroBossRestaurantDB").collection("carts")
+
+    // jwt related token
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign( user, process.env.SECRET_ACCESS_TOKEN, {expiresIn: '1h'})
+      res.send({token})
+    })
 
 
     // user related apis
