1c8837913539650c3a33b93db16eaf3e890c8529
AkashPaul923 <akashpaul923@gmail.com>
2025-01-16 12:54:59 +0600

    user add in db conditionaly

--- index.js
+++ index.js
@@ -37,12 +37,20 @@ async function run() {
     // user related apis
     app.post('/users', async (req, res) => {
       const user = req.body
+
+      // check user exist or not
+      const query = {email : user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message : 'user already exist', insertedId: null })
+      }
+      
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
 
 
-    
+
     // menu apis
     app.get('/menu', async (req, res) => {
         const result = await menuCollection.find().toArray()
